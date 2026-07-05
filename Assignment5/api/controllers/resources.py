44259eb0e229404e2ed71d46ed94feab7b948310
Nathaniel Atkins <natkins6@charlotte.edu>
2026-07-05 13:53:18 -0400

    Revised comments in resources and sandwiches controllers

--- Assignment5/api/controllers/resources.py
+++ Assignment5/api/controllers/resources.py
@@ -4,18 +4,18 @@ from ..models import models, schemas
 
 
 def create(db: Session, resource):
-    # Create a new instance of the Order model with the provided data
+    # Create a new instance of the Resource model with the provided data
     db_resource = models.Resource(
         item=resource.item,
         amount=resource.amount
     )
-    # Add the newly created Order object to the database session
+    # Add the newly created Resource object to the database session
     db.add(db_resource)
     # Commit the changes to the database
     db.commit()
-    # Refresh the Order object to ensure it reflects the current state in the database
+    # Refresh the Resource object to ensure it reflects the current state in the database
     db.refresh(db_resource)
-    # Return the newly created Order object
+    # Return the newly created Resource object
     return db_resource
 
 
@@ -28,20 +28,20 @@ def read_one(db: Session, resource_id):
 
 
 def update(db: Session, resource_id, resource):
-    # Query the database for the specific order to update
+    # Query the database for the specific Resource to update
     db_resource = db.query(models.Resource).filter(models.Resource.id == resource_id)
-    # Extract the update data from the provided 'order' object
+    # Extract the update data from the provided 'resource' object
     update_data = resource.model_dump(exclude_unset=True)
     # Update the database record with the new data, without synchronizing the session
     db_resource.update(update_data, synchronize_session=False)
     # Commit the changes to the database
     db.commit()
-    # Return the updated order record
+    # Return the updated resource record
     return db_resource.first()
 
 
 def delete(db: Session, resource_id):
-    # Query the database for the specific order to delete
+    # Query the database for the specific resource to delete
     db_resource = db.query(models.Resource).filter(models.Resource.id == resource_id)
     # Delete the database record without synchronizing the session
     db_resource.delete(synchronize_session=False)
