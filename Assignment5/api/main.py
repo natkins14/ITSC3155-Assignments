19a107625c7c52412dd9b86d74b70e7ac5f93999
Nathaniel Atkins <natkins6@charlotte.edu>
2026-07-05 12:19:15 -0400

    Implemented CRUD controller and endpoints for sandwiches

--- Assignment5/api/main.py
+++ Assignment5/api/main.py
@@ -1,6 +1,7 @@
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 from fastapi.middleware.cors import CORSMiddleware
+from .controllers import sandwiches as sandwiches_controller
 
 from .models import models, schemas
 from .controllers import orders
@@ -53,3 +54,36 @@ def delete_one_order(order_id: int, db: Session = Depends(get_db)):
     if order is None:
         raise HTTPException(status_code=404, detail="User not found")
     return orders.delete(db=db, order_id=order_id)
+
+@app.post("/sandwiches/", response_model=schemas.Sandwich, tags=["Sandwiches"])
+def create_sandwich(sandwich: schemas.SandwichCreate, db: Session = Depends(get_db)):
+    return sandwiches_controller.create(db=db, sandwich=sandwich)
+
+
+@app.get("/sandwiches/", response_model=list[schemas.Sandwich], tags=["Sandwiches"])
+def read_sandwiches(db: Session = Depends(get_db)):
+    return sandwiches_controller.read_all(db)
+
+
+@app.get("/sandwiches/{sandwich_id}", response_model=schemas.Sandwich, tags=["Sandwiches"])
+def read_one_sandwich(sandwich_id: int, db: Session = Depends(get_db)):
+    db_sandwich = sandwiches_controller.read_one(db, sandwich_id=sandwich_id)
+    if db_sandwich is None:
+        raise HTTPException(status_code=404, detail="Sandwich not found")
+    return db_sandwich
+
+
+@app.put("/sandwiches/{sandwich_id}", response_model=schemas.Sandwich, tags=["Sandwiches"])
+def update_one_sandwich(sandwich_id: int, sandwich: schemas.SandwichUpdate, db: Session = Depends(get_db)):
+    db_sandwich = sandwiches_controller.read_one(db, sandwich_id=sandwich_id)
+    if db_sandwich is None:
+        raise HTTPException(status_code=404, detail="Sandwich not found")
+    return sandwiches_controller.update(db=db, sandwich_id=sandwich_id, sandwich=sandwich)
+
+
+@app.delete("/sandwiches/{sandwich_id}", tags=["Sandwiches"])
+def delete_one_sandwich(sandwich_id: int, db: Session = Depends(get_db)):
+    db_sandwich = sandwiches_controller.read_one(db, sandwich_id=sandwich_id)
+    if db_sandwich is None:
+        raise HTTPException(status_code=404, detail="Sandwich not found")
+    return sandwiches_controller.delete(db=db, sandwich_id=sandwich_id)
